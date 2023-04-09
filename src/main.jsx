fe2ddbda04a0ee3a07aeab8b373ed409f8eaf19d
Md Zihad Shah <freelancerjoy10@gmail.com>
2023-04-10 02:08:28 +0600

    test

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Books from "./Components/Books";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "books",
+				element: <Books />,
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</React.StrictMode>,
+);
